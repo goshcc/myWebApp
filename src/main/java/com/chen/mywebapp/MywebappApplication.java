c155477e2cc4d91a282f7faf6158bd9050e0fae1
Gao Chen <297883144@qq.com>
2018-10-20 17:00:18 +0800

    Create MywebappApplication.java

---
+++ src/main/java/com/chen/mywebapp/MywebappApplication.java
@@ -0,0 +1,11 @@
+package com.chen.mywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebappApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MywebappApplication.class, args);
+	}
+}
